99a0b0aa901e572c22612e1dda51191a70987fc8
lab unilago <lab@unilago.com>
2026-03-09 21:51:22 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { engine } = require("express-handlebars")
 const bodyParser = require('body-parser')
+const path = require("path")
 
 const app = express()
 
@@ -9,6 +10,9 @@ app.use(bodyParser.json())
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'views/css/')))
+app.use(express.static(path.join(__dirname, 'views/js/')))
 
 app.get("/", (req, res) => {
     res.render('index')

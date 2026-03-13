f11b3f7a73962dc6e097393844776afa06a31ec2
Enzo Crispim <enzzocrispim@icloud.com>
2026-03-12 21:34:48 -0300

    atualizando index.js

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 const express = require("express")
-<<<<<<< HEAD
 const { engine } = require("express-handlebars")
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 const path = require("path")
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.engine('handlebars', engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", engine({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
-app.use(express.static(path.join(__dirname, 'views/css/'))) // Indicando pasta de arquivos estáticos css, pode ser usado para js e outros.
-app.use(express.static(path.join(__dirname, 'views/js/')))
+app.use(express.static(path.join(__dirname, "views/css/")))
+app.use(express.static(path.join(__dirname, "views/js/")))
 
 app.get("/", (req, res) => {
-    res.render('index')
+    res.render("index")
 })
 
-app.get("/sobre/{:id}", (req, res) => {
-
+app.get("/sobre/:id", (req, res) => {
     const id = req.params.id
-    
     res.render("sobre", { id })
 })
 
 app.listen(3000, () => {
-    console.log('Servidor rodando http://localhost:3000')
-=======
-
-const app = express()
-
-app.use("/", function (req, res){
-    res.send("<h1>Funcionou<h1>")
-})
-
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
->>>>>>> ea5721f7253ef3b2ffe41e5a1a5ace33bb1ac3c2
+    console.log("Servidor rodando em http://localhost:3000")
 })

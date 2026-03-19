bda31ccec4889bd75f1c239bfbe0e2a4bc60adfe
Enzo Crispim <enzzocrispim@icloud.com>
2026-03-18 23:29:48 -0300

    trabalho finalizado

--- route.js
+++ route.js
@@ -15,3 +15,14 @@ router.get("/contato", (req, res) => {
     res.render("contato")
 })
 
+router.post("/contato", (req, res) => {
+    const nome = req.body.nome
+    const email = req.body.email
+    const mensagem = req.body.mensagem
+
+    res.render("sucesso", {
+        nome,
+        email,
+        mensagem
+    })
+})
